addb06c97f32e08fa5b5e30d39416a0acb53ded1
MykhailoMishchenko <mishchenko.mykhailo@gmail.com>
2022-02-03 16:38:55 +0200

    create endpoint for user profile in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,6 +57,35 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 })
 
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        })
+    } else {
+        res.status(404)
+        throw new Error('Користувач не знайдений')
+    }
+})
+
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
@@ -76,4 +105,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export {authUser, registerUser, getUserProfile}
+export {authUser, registerUser, getUserProfile, updateUserProfile}
